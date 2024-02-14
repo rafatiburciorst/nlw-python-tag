6ac2b19d8a987414d0578e588372405b0274912a
Rafael Tiburcio <rafael.tiburcio@live.com>
2024-02-14 20:39:40 -0300

    feat: finish

---
+++ src/errors/error_types/http_unprocessable_entity.py
@@ -0,0 +1,7 @@
+class UnprrocessableEntityError(Exception):
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+        self.message = message
+        self.name = "UnprocessableEntity"
+        self.status_code = 422
